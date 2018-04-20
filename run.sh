0abd6cd5b52e5fac5ecd1b88996a752152aff72b
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2018-04-20 15:22:54 -0400

    Can be run from command line

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp "target/*;target/lib/*" com.fantabel.jchip8.App $@
--- src/main/java/com/fantabel/jchip8/App.java
+++ src/main/java/com/fantabel/jchip8/App.java
@@ -1,13 +1,13 @@
 package com.fantabel.jchip8;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" + args[0] + " " + StringUtils.isEmpty(args[0]) );
     }
 }
