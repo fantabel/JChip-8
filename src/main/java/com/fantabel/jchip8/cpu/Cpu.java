5c1e82fa9179876f6ceb1a33b37d3466f4f7dc89
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2018-04-23 15:44:25 -0400

    More work on register and memory

--- src/main/java/com/fantabel/jchip8/cpu/Cpu.java
+++ src/main/java/com/fantabel/jchip8/cpu/Cpu.java
@@ -22,11 +22,24 @@ public class Cpu {
 
 		logger.trace("Initializing registers");
 		registers = new byte[NUMBER_OF_REGISTERS];
-
+		registers = generateRandomRegisters();
 		logger.trace("Initializing memory");
 		memory = new byte[MEMORY_LENGTH];
 
 		logger.traceExit();
+	}
+
+	private byte getRandomByte() {
+		int range = Byte.MAX_VALUE - Byte.MIN_VALUE;
+		return (byte)((Math.random() * range) + Byte.MIN_VALUE);
+	}
+
+	private byte[] generateRandomRegisters() {
+		byte[] tempBytes = new byte[NUMBER_OF_REGISTERS];
+		for (int i = 0 ; i < tempBytes.length ; i++) {
+			tempBytes[i] = getRandomByte();
+		}
+		return tempBytes;
 	}
 
 	public String dump() {
@@ -34,12 +47,27 @@ public class Cpu {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("Registers\n");
-		int i = 0;
-		for (byte b : registers)
-			sb.append("v" + Integer.toHexString(i++).toUpperCase() + ": " + String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0') + "\n");
-		
+		sb.append(dumpRegisters());
+
 		sb.append("\nMemory\n");
-		i = 0;
+		//sb.append(dumpMemory());
+		
+		return logger.traceExit(sb.toString());
+	}
+
+	public String dumpRegisters() {
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+		StringBuilder sbTemp = new StringBuilder();
+		for (byte b : registers) {
+			sb.append("V" + String.format("%x", i++) + ": " + String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0') + "\n");
+		}
+		return sb.toString();
+	}
+
+	public String dumpMemory() {
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
 		for (byte b : memory) {
 			if (i % 16 == 0)
 				sb.append(String.format("%4s", Integer.toHexString(i)).toUpperCase().replace(' ', '0') + " ");
@@ -47,7 +75,6 @@ public class Cpu {
 			if (++i % 16 == 0)
 				sb.append('\n');
 		}
-		return logger.traceExit(sb.toString());
+		return sb.toString();
 	}
-	
 }
