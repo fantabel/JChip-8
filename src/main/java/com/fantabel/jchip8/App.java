f5d0b18412e96cc4393c7edd14e1262728622ad9
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2018-04-23 22:55:48 -0400

    Beautifier

--- src/main/java/com/fantabel/jchip8/App.java
+++ src/main/java/com/fantabel/jchip8/App.java
@@ -9,8 +9,16 @@ import com.fantabel.jchip8.cpu.Cpu;
  *
  */
 public class App {
-    public static void main( String[] args ) {
+
+	    public static void main( String[] args ) {
 		Cpu cpu = new Cpu();
 		cpu.dump();
+
+/*
+		for (int i = 0 ; i <= 255 ; i++)
+			System.out.println(i + " " + (byte)i + " " + convertByteToChar(i));
+		for (byte b = Byte.MIN_VALUE ; b < Byte.MAX_VALUE ; b++)
+			System.out.println((b & 0xFF) + " " + Integer.toBinaryString(b & 0xFF) + ": " + (char)(b & 0xFF));
+*/
     }
 }
--- src/main/java/com/fantabel/jchip8/cpu/Cpu.java
+++ src/main/java/com/fantabel/jchip8/cpu/Cpu.java
@@ -22,24 +22,33 @@ public class Cpu {
 
 		logger.trace("Initializing registers");
 		registers = new byte[NUMBER_OF_REGISTERS];
-		registers = generateRandomRegisters();
+		Cpu.fillWithRandomData(registers);
 		logger.trace("Initializing memory");
 		memory = new byte[MEMORY_LENGTH];
+		Cpu.fillWithRandomData(memory);
 
 		logger.traceExit();
 	}
 
-	private byte getRandomByte() {
+	private static byte getRandomByte() {
 		int range = Byte.MAX_VALUE - Byte.MIN_VALUE;
 		return (byte)((Math.random() * range) + Byte.MIN_VALUE);
 	}
 
-	private byte[] generateRandomRegisters() {
-		byte[] tempBytes = new byte[NUMBER_OF_REGISTERS];
-		for (int i = 0 ; i < tempBytes.length ; i++) {
-			tempBytes[i] = getRandomByte();
-		}
-		return tempBytes;
+	//TODO Move to a util class
+	private static void fillWithRandomData(byte[] array) {
+		for (int i = 0 ; i < array.length ; i++)
+			array[i] = getRandomByte();
+	}
+	
+	public static boolean isCharPrintable(int i) {
+		return (i >= 32 && i <= 126 ||
+			i >= 161 && i <= 172 ||
+			i >= 174 && i <= 255);
+	}
+
+	public static char convertByteToChar(int i) {
+		return isCharPrintable(i) ? (char)i : '.';
 	}
 
 	public String dump() {
@@ -50,7 +59,7 @@ public class Cpu {
 		sb.append(dumpRegisters());
 
 		sb.append("\nMemory\n");
-		//sb.append(dumpMemory());
+		sb.append(dumpMemory());
 		
 		return logger.traceExit(sb.toString());
 	}
@@ -60,20 +69,26 @@ public class Cpu {
 		int i = 0;
 		StringBuilder sbTemp = new StringBuilder();
 		for (byte b : registers) {
-			sb.append("V" + String.format("%x", i++) + ": " + String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0') + " " + String.format("%2s", Integer.toHexString(b & 0xFF)).replace(' ', '0').toUpperCase() + "\n");
+			sb.append("V" + String.format("%x", i++) + ": " + String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0') + " " + String.format("%02X", b) + "\n");
+			//sb.append("V" + String.format("%x", i++) + ": " + String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0') + " " + String.format("%2s", Integer.toHexString(b & 0xFF)).replace(' ', '0').toUpperCase() + "\n");
 		}
 		return sb.toString();
 	}
 
 	public String dumpMemory() {
 		StringBuilder sb = new StringBuilder();
+		StringBuilder sbTemp = new StringBuilder();
 		int i = 0;
 		for (byte b : memory) {
 			if (i % 16 == 0)
-				sb.append(String.format("%4s", Integer.toHexString(i)).toUpperCase().replace(' ', '0') + " ");
-			sb.append(b);
-			if (++i % 16 == 0)
+				sb.append(String.format("%04X ", i));
+			sbTemp.append(convertByteToChar(b));
+			sb.append(String.format("%02X ", b));
+			if (++i % 16 == 0) {
+				sb.append(sbTemp.toString());
+				sbTemp = new StringBuilder();
 				sb.append('\n');
+			}
 		}
 		return sb.toString();
 	}
